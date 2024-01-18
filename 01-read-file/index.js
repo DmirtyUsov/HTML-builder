07fab7d1888092fd494056b4819a255e96ffa1b5
Dima Usov <dmitry.usov@yahoo.com>
2024-01-18 12:33:19 +0300

    fix: remove redundant code

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -4,16 +4,4 @@ const path = require('path');
 const fileToRead = path.join(__dirname, 'text.txt');
 const readStream = fs.createReadStream(fileToRead, {encoding: 'utf8'});
 
-// Option 1
-// readStream.on('data', chunk => {
-//   console.log(chunk);
-// })
-
-// Option 2
 readStream.pipe(process.stdout);
-
-// exit on uncaught errors
-process.on('uncaughtException', err => {
-  console.log(`Uncaught error: ${err}`);
-  process.exit(1);
-})
